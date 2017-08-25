5fcfce3a9b72e05bc2b52ef1e5a950a7be80070d
Piper Merriam <pipermerriam@gmail.com>
2017-08-25 16:35:43 -0600

    flake8

--- web3/utils/module_testing/eth_module.py
+++ web3/utils/module_testing/eth_module.py
@@ -151,7 +151,7 @@ class EthModuleTest(object):
 
     def test_eth_sendRawTransaction(self, web3, funded_account_for_raw_txn):
         txn_hash = web3.eth.sendRawTransaction(
-            '0xf8648085174876e8008252089439eeed73fb1d3855e90cbd42f348b3d7b340aaa601801ba0ec1295f00936acd0c2cb90ab2cdaacb8bf5e11b3d9957833595aca9ceedb7aada05dfc8937baec0e26029057abd3a1ef8c505dca2cdc07ffacb046d090d2bea06a'
+            '0xf8648085174876e8008252089439eeed73fb1d3855e90cbd42f348b3d7b340aaa601801ba0ec1295f00936acd0c2cb90ab2cdaacb8bf5e11b3d9957833595aca9ceedb7aada05dfc8937baec0e26029057abd3a1ef8c505dca2cdc07ffacb046d090d2bea06a'  # noqa: E501
         )
         assert txn_hash == '0x1f80f8ab5f12a45be218f76404bda64d37270a6f4f86ededd0eb599f80548c13'
 
